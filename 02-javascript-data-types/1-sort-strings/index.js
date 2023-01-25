2f94ccb05bf58b9726a596537008531a45e70bf4
svetlana.goryacheva <svetlana.goryacheva@gmail.com>
2023-01-25 18:25:27 +0400

    02

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,17 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr = [], param = 'asc') {
+  const directions = {
+    asc: 1,
+    desc: -1
+  };
+  const direction = directions[param];
 
+  if(typeof direction === 'undefined') {
+    throw new Error('not direction');
+  }
+  return [...arr].sort((str1, str2) => {
+    return direction * str1.localeCompare(str2, ['ru','en'], {caseFirst: 'upper'});
+ });
 }
